b9a131563635cdec1b0d220236c131adca712ab2
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-12-31 17:34:33 -0300

    chore(web): rename SockerContext to SocketContext

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -6,7 +6,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 
 import type { ReactNode } from 'react';
 
-import { SocketProvider } from '@/contexts/SockerContext';
+import { SocketProvider } from '@/contexts/SocketContext';
 import { queryClient } from '@/libs/queryClient';
 
 export function Providers({ children }: { children: ReactNode }) {
