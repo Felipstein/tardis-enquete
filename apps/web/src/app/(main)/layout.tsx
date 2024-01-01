d37f39ced10db9a9c0a591ed82f3122f745ed1a8
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-12-31 21:53:26 -0300

    chore(web): add debug message about polls

--- apps/web/src/app/(main)/layout.tsx
+++ apps/web/src/app/(main)/layout.tsx
@@ -22,6 +22,10 @@ export default function MainLayout({ children }: { children: ReactNode }) {
         <main className="h-fit">{children}</main>
       </div>
 
+      <span className="fixed bottom-4 left-1/2 z-50 -translate-x-1/2 rounded-md border border-red-500 bg-red-800/30 px-3.5 py-2 text-xs font-semibold text-red-500">
+        As votações estão bloqueadas
+      </span>
+
       <Footer />
     </div>
   );
