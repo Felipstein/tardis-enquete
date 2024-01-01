756034b697f73498fd49148f4b18a67ea21052f3
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-01-01 18:22:36 -0300

    chore(server): other improves to discord error logger

--- apps/server/src/services/DiscordService.ts
+++ apps/server/src/services/DiscordService.ts
@@ -33,7 +33,17 @@ export default class DefaultService {
       (error) => {
         if (error instanceof AxiosError) {
           console.error(chalk.red('Discord Request throwed an error:'));
-          console.error(JSON.stringify(error.toJSON(), null, 2));
+          console.error(
+            JSON.stringify(
+              {
+                headers: error.response?.headers,
+                body: error.response?.data,
+                response: error.response,
+              },
+              null,
+              2,
+            ),
+          );
 
           const responseData = error.response?.data;
 
