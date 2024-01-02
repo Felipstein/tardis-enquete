3ed34b3f72629d752ed9fefbe367ede5f54ce904
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-01-01 22:37:05 -0300

    chore: remove scripts

--- apps/server/src/utils/getHostURLInRequest.ts
+++ apps/server/src/utils/getHostURLInRequest.ts
@@ -3,11 +3,7 @@ import { Request } from 'express';
 export function getHostURLInRequest(req: Request) {
   const hostname = req.headers.host ?? req.get('host') ?? `localhost:${process.env.PORT}`;
 
-  console.log('HTTPS CHECK:', process.env.HTTTPS, req.protocol);
-
   const hostURL = `${process.env.HTTPS ? 'https' : req.protocol}://${hostname}`;
-
-  console.log('URL RETORNED:', hostURL);
 
   return hostURL;
 }
