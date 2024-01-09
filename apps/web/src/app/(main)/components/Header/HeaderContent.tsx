ff60d0019e4355e5c48b5fc3474a371d99782bed
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-01-09 00:29:24 -0300

    fix(web): put the header in the center

--- apps/web/src/app/(main)/components/Header/HeaderContent.tsx
+++ apps/web/src/app/(main)/components/Header/HeaderContent.tsx
@@ -5,7 +5,7 @@ import { LogoImage } from '@/app/components/LogoImage';
 
 export function HeaderContent() {
   return (
-    <main className="flex h-full w-full max-w-[1980px] items-center justify-between p-4 sm:px-10 sm:py-6">
+    <main className="mx-auto flex h-full w-full max-w-[1980px] items-center justify-between p-4 sm:px-10 sm:py-6">
       <div className="flex items-center gap-2.5">
         <LogoImage width={42} height={42} />
 
