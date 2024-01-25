4c010a51a4cb427bd1217d171085e9bb31b34af6
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-01-25 18:59:11 -0300

    chore: remove initial fetch of polls

--- apps/web/src/app/(main)/page.tsx
+++ apps/web/src/app/(main)/page.tsx
@@ -1,10 +1,8 @@
 import { redirect } from 'next/navigation';
 
-import { PollTimeline } from '@tardis-enquete/contracts';
 import { PollsList } from './components/PollsList';
 
 import { getAccessTokenServerSide } from '@/utils/getAccessTokenServerSide';
-import { pollService } from '@/services/api/pollService';
 
 export default async function HomePage() {
   const accessToken = getAccessTokenServerSide();
@@ -13,22 +11,9 @@ export default async function HomePage() {
     return redirect('/login');
   }
 
-  let polls: PollTimeline[] | undefined;
-  let errorOnFetchPolls: unknown | null | undefined;
-
-  try {
-    polls = await pollService.getPolls();
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      errorOnFetchPolls = { ...error };
-    } else {
-      errorOnFetchPolls = error;
-    }
-  }
-
   return (
     <div className="mt-6 flex h-full items-center justify-center sm:mt-8">
-      <PollsList pollsAlreadyFetched={polls} errorOnInitialFetch={errorOnFetchPolls} />
+      <PollsList />
     </div>
   );
 }
